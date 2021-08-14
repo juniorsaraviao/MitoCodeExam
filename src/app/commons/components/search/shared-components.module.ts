1454766edb0005325e1d0ac9aef020478a4563ec
Junior Saravia <junior.saravia.o@uni.pe>
2021-08-14 01:12:37 -0500

    Update shared module

--- src/app/commons/components/search/shared-components.module.ts
+++ src/app/commons/components/search/shared-components.module.ts
@@ -4,13 +4,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MAT_PAGINATOR_PROVIDER } from '../../services/local/custom-paginator.service';
 import { SearchComponent } from './search.component';
 
 @NgModule({
 	declarations: [SearchComponent],
 	imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatIconModule],
-	exports: [SearchComponent],
-	providers: [MAT_PAGINATOR_PROVIDER]
+	exports: [SearchComponent]
 })
 export class SharedComponentsModule {}
